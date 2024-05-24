b3a61c708513f06794a1e91703c567d1ec68c691
Erol Dalkiran <eroldalkiran0@gmail.com>
2024-05-24 22:23:54 +0300

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,11 @@
+public class Runner {
+
+    public static void main(String[] args) {
+
+
+        //versiyon 1
+        System.out.println("Hello git");
+
+        System.out.println("git add . staging area ya alir.");
+    }
+}
