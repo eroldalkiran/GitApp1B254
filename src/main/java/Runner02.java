68e2bdb9d21f6ba7a5ffd9067243b5c4cb7e290c
Erol Dalkiran <eroldalkiran0@gmail.com>
2024-05-25 22:34:29 +0300

    v4

--- src/main/java/Runner02.java
+++ src/main/java/Runner02.java
@@ -2,5 +2,8 @@ public class Runner02 {
     public static void main(String[] args) {
         //versiyon 3
         System.out.println("hello");
+
+        // versiyon 4
+        System.out.println("");
     }
 }
